77a146f6615523dd88f67c1ade9a552eda3fffe2
muzardemoses <muzardemoses@gmail.com>
2023-06-15 00:54:04 +0100

    Implementing messaging functionality

--- src/Pages/Messaging/ChatRoom.tsx
+++ src/Pages/Messaging/ChatRoom.tsx
@@ -1,6 +1,33 @@
+import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { selectUser } from "../../Config/userSlice";
+import { useState } from 'react';
+import {
+    serverTimestamp,
+    setDoc,
+    doc,
+    collection,
+    query,
+    where,
+    orderBy,
+    onSnapshot,
+    addDoc,
+    getDocs,
+} from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+
+
 export const ChatRoom = () => {
+    const loggedInUser = useSelector(selectUser);
+    const {idone, idtwo} = useParams<{idone: string, idtwo: string}>()
+    const [message, setMessage] = useState('')
+    const chatUser = loggedInUser?.id === idone ? idtwo : idone
+
+
+
+
     return (
-        <div className=" h-[1000px] ">
+        <div className="">
             room
         </div>
     )
