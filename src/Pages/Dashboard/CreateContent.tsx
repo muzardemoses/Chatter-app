d992ef82b3b474ebba04f5e5da3b079699353272
muzardemoses <muzardemoses@gmail.com>
2023-06-21 18:48:34 +0100

    finalize creating content functionalities

--- src/Pages/Dashboard/CreateContent.tsx
+++ src/Pages/Dashboard/CreateContent.tsx
@@ -11,8 +11,10 @@ import {
     addDoc,
     getDocs,
 } from "firebase/firestore";
+import { toast } from 'react-toastify'
 import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { db, storage } from '../../Config/firebase';
+import { useNavigate } from 'react-router-dom'
 import addSVG from '../../assets/Svg/Feed/outline-add.svg'
 import closeSVG from '../../assets/Svg/Feed/outline-close.svg'
 import imageSVG from '../../assets/Svg/Feed/image-outline.svg'
@@ -28,11 +30,130 @@ export const CreateContent = () => {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
-    const [image, setImage] = useState<File | null>(null)
+    const [imageFiles, setImageFiles] = useState<File[]>([]);
+    const [videoFiles, setVideoFiles] = useState<File[]>([]);
+    const [mediaPreviewUrls, setMediaPreviewUrls] = useState<string[]>([]);
+
+
+
+
+    const postId = `${loggedInUser?.id}-${Date.now()}`;
+    const timestamp = serverTimestamp();
+
+    const handleImageFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = Array.from(e.target.files || []);
+        const previewUrls: string[] = [];
+        const newFiles: File[] = [];
+
+        files.forEach((file) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                previewUrls.push(reader.result as string);
+                if (previewUrls.length === files.length) {
+                    setMediaPreviewUrls([...mediaPreviewUrls, ...previewUrls]);
+                }
+            };
+            reader.readAsDataURL(file);
+            newFiles.push(file);
+        });
+
+        setImageFiles([...imageFiles, ...newFiles]);
+    };
+
+    const handleVideoFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = Array.from(e.target.files || []);
+        const previewUrls: string[] = [];
+        const newFiles: File[] = [];
+
+        files.forEach((file) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                previewUrls.push(reader.result as string);
+                if (previewUrls.length === files.length) {
+                    setMediaPreviewUrls([...mediaPreviewUrls, ...previewUrls]);
+                }
+            };
+            reader.readAsDataURL(file);
+            newFiles.push(file);
+        });
+
+        setVideoFiles([...videoFiles, ...newFiles]);
+    };
+
+    const cancelMediaFile = (index: number) => {
+        const updatedPreviewUrls = [...mediaPreviewUrls];
+        updatedPreviewUrls.splice(index, 1);
+        setMediaPreviewUrls(updatedPreviewUrls);
+
+        if (index < imageFiles.length) {
+            const updatedImageFiles = [...imageFiles];
+            updatedImageFiles.splice(index, 1);
+            setImageFiles(updatedImageFiles);
+        } else {
+            const updatedVideoFiles = [...videoFiles];
+            const videoIndex = index - imageFiles.length;
+            updatedVideoFiles.splice(videoIndex, 1);
+            setVideoFiles(updatedVideoFiles);
+        }
+    };
+
+
+    const createPost = async () => {
+        const postRef = doc(db, "posts", postId);
+
+        // Upload media files and get their download URLs
+        // Upload image files and get their download URLs
+        const imageDownloadUrls = await Promise.all(
+            imageFiles.map(async (file) => {
+                const imageRef = ref(storage, `images/${postId}/${file.name}`);
+                await uploadBytes(imageRef, file);
+                const downloadUrl = await getDownloadURL(imageRef);
+                return downloadUrl;
+            })
+        );
+
+        // Upload video files and get their download URLs
+        const videoDownloadUrls = await Promise.all(
+            videoFiles.map(async (file) => {
+                const videoRef = ref(storage, `videos/${postId}/${file.name}`);
+                await uploadBytes(videoRef, file);
+                const downloadUrl = await getDownloadURL(videoRef);
+                return downloadUrl;
+            })
+        );
+
+        await setDoc(postRef, {
+            id: postId,
+            title: title,
+            content: content,
+            cretorId: loggedInUser?.id,
+            creatorName: loggedInUser?.displayName,
+            timestamp: timestamp,
+            likes: [],
+            comments: [],
+            shares: [],
+            views: [],
+            isDeleted: false,
+            bookmarkedBy: [],
+            media: {
+                images: imageDownloadUrls,
+                videos: videoDownloadUrls,
+            },
+        });
+        setTitle("")
+        setContent("")
+        setImageFiles([])
+        setVideoFiles([])
+        setMediaPreviewUrls([])
+        toast.success("Post created successfully")
+    }
+
     return (
         <div className="h-full p-8">
             <div className="w-full h-full pt-9 pl-20 pr-36 border border-gray-300 rounded-lg flex flex-col gap-10">
-                <button className="self-end bg-blue-700 text-white w-36 py-4 rounded-lg text-base font-medium">
+                <button
+                    onClick={createPost}
+                    className="self-end bg-blue-700 text-white w-36 py-4 rounded-lg text-base font-medium">
                     Publish
                 </button>
                 <div className="w-full flex gap-9">
@@ -48,19 +169,64 @@ export const CreateContent = () => {
                         {showMediaOptions &&
                             <div className='flex items-center h-full gap-7'>
                                 <p className='h-full border-l border-gray-200'></p>
-                                <button className='p-4 border border-blue-600 rounded-full h-max w-max'>
-                                    <img src={imageSVG}
-                                        alt="image"
-                                        className="w-6 h-6"
-                                    />
-                                </button>
-                                <button className='p-4 border border-blue-600 rounded-full h-max w-max'>
-                                    <img src={videoSVG}
-                                        alt="video"
-                                        className="w-6 h-6"
-                                    />
-                                </button>
-                            </div>}
+                                <label htmlFor="image-input">
+                                    <button className='p-4 border border-blue-600 rounded-full h-max w-max'>
+                                        <img src={imageSVG} alt="image" className="w-6 h-6" />
+                                    </button>
+                                </label>
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={handleImageFileChange}
+                                    className="hiddn"
+                                    id="image-input"
+                                    multiple
+                                />
+                                <label htmlFor="video-input">
+                                    <button className='p-4 border border-blue-600 rounded-full h-max w-max'>
+                                        <img src={videoSVG} alt="video" className="w-6 h-6" />
+                                    </button>
+                                </label>
+                                <input
+                                    type="file"
+                                    accept="video/*"
+                                    onChange={handleVideoFileChange}
+                                    className="hidde"
+                                    id="video-input"
+                                    multiple
+                                />
+
+
+                                <div className="flex flex-wrap gap-4">
+                                    {mediaPreviewUrls.map((url, index) => (
+                                        <div key={index} className="relative">
+                                            {url.includes("video") ? (
+                                                <video
+                                                    src={url}
+                                                    className="w-32 h-32 object-cover rounded-lg"
+                                                    controls
+                                                />
+                                            ) : (
+                                                <img
+                                                    src={url}
+                                                    alt={`Preview ${index}`}
+                                                    className="w-32 h-32 object-cover rounded-lg"
+                                                />
+                                            )
+
+                                            }
+
+                                            <button
+                                                onClick={() => cancelMediaFile(index)}
+                                                className="absolute top-0 right-0 w-6 h-6 bg-red-500 rounded-full flex items-center justify-center text-white"
+                                            >
+                                                X
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        }
                     </div>
                     <div className={`w-full flex flex-col gap-3 ${showMediaOptions ? "hidden" : "block"}`}>
                         <input
@@ -82,3 +248,4 @@ export const CreateContent = () => {
         </div>
     )
 }
+
